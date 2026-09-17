3b7440dd37eaf16d504d9fb40058e76fe1909986
khrystynafesechko-dotcom <khrystynafesechko@gmail.com>
2026-09-17 15:41:50 +0300

    Update campaigns_stg.sql

--- sql/data_quality/campaigns_stg.sql
+++ sql/data_quality/campaigns_stg.sql
@@ -1,4 +1,3 @@
-
 ---Checking for missing values (NULLs / empty strings).
 SELECT
   COUNTIF(campaign_id IS NULL) AS null_campaign_id,
@@ -8,3 +7,29 @@ SELECT
   COUNTIF(duration_days IS NULL) AS null_duration,
   COUNTIF(total_budget IS NULL) AS null_budget
 FROM `payment_dwh.campaigns_stg`;
+
+---Duplicate check for campaign_id
+SELECT campaign_id, COUNT(*) AS cnt
+FROM `payment_dwh.campaigns_stg`
+GROUP BY campaign_id
+HAVING cnt > 1;
+
+---Date Logical Consistency Check
+SELECT campaign_id, start_date, end_date
+FROM `payment_dwh.campaigns_stg`
+WHERE end_date < start_date;
+
+---Checked that duration_days matches the difference between start_date and end_date.
+SELECT
+  campaign_id,
+  start_date,
+  end_date,
+  duration_days,
+  DATE_DIFF(end_date, start_date, DAY) AS calc_duration
+FROM `payment_dwh.campaigns_stg`
+WHERE duration_days != DATE_DIFF(end_date, start_date, DAY);
+
+---Budget Accuracy Check
+SELECT campaign_id, total_budget
+FROM `payment_dwh.campaigns_stg`
+WHERE total_budget <= 0;
